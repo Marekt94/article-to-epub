2abab54651d512b6215b4fe298a1643b161f426e
Marekt94 <marekt94@gmail.com>
2026-04-29 12:12:00 +0200

    convert html endpoint implemented, postman collection updated

--- cmd/article-to-epub-service/ModuleArticleToEpub.go
+++ cmd/article-to-epub-service/ModuleArticleToEpub.go
@@ -7,8 +7,11 @@ import (
 	"article-to-epub/pkg/modules/emailsender"
 	"article-to-epub/pkg/modules/htmltoepubconverter"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 
+	"github.com/Marekt94/go-kernel-mt/logging"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,42 +20,29 @@ const (
 	body  = "Article from article-to-epub converter"
 )
 
-type Request struct {
+type RequestUrl struct {
 	Url   string `json:"url"`
 	Email string `json:"email"`
-}
-
-type Response struct {
-	Sent bool `json:"sent"`
 }
 
 type ModuleArticleToEpub struct {
 	server *gin.Engine
 }
 
-func (m *ModuleArticleToEpub) convertHtml(c *gin.Context) {
+func (m *ModuleArticleToEpub) convertArticle(c *gin.Context,
+	article []byte,
+	articleName string,
+	receiverEmail string,
+	a modules.ArticleSimplifierIntf,
+	h modules.HtmlToEpubConverterIntf,
+	e modules.EmailSenderIntf) {
 
-}
-
-func (m *ModuleArticleToEpub) fetchUrl(c *gin.Context) {
-	var req Request
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	var a modules.ArticleSimplifierIntf
-
-	a = &articlesimplifier.ArticleSimplifierFromURL{}
-	html, title, author, err := a.SimplifyArticle([]byte(req.Url))
+	html, title, author, err := a.SimplifyArticle(article)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	var h modules.HtmlToEpubConverterIntf
-
-	h = &htmltoepubconverter.HtmlToEpubConverter{}
 	var epub []byte
 	epub, err = h.ConvertHtmlToEpub(html, title, author)
 	if err != nil {
@@ -60,17 +50,16 @@ func (m *ModuleArticleToEpub) fetchUrl(c *gin.Context) {
 		return
 	}
 
-	attachmentName := misc.AdaptUrlToFileName(req.Url) + `.epub`
+	attachmentName := articleName + `.epub`
 
-	if req.Email == "" {
+	if receiverEmail == "" {
 		c.Header("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, attachmentName))
 		c.Data(http.StatusOK, "application/epub+zip", epub)
 		return
 	}
 
-	var e modules.EmailSenderIntf
 	e = emailsender.NewEmailSender("")
-	err = e.SendEmail([]string{req.Email},
+	err = e.SendEmail([]string{receiverEmail},
 		fmt.Sprintf(topic, attachmentName),
 		body,
 		attachmentName,
@@ -82,6 +71,57 @@ func (m *ModuleArticleToEpub) fetchUrl(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
+}
+
+func (m *ModuleArticleToEpub) fetchUrl(c *gin.Context) {
+	var req RequestUrl
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	articleName := misc.AdaptUrlToFileName(req.Url)
+	m.convertArticle(c, []byte(req.Url), articleName, req.Email,
+		&articlesimplifier.ArticleSimplifierFromURL{}, &htmltoepubconverter.HtmlToEpubConverter{},
+		emailsender.NewEmailSender(""))
+}
+
+func (m *ModuleArticleToEpub) convertHtml(c *gin.Context) {
+	fh, err := c.FormFile(`html`)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	receiverEmail := c.PostForm(`email`)
+	url := c.PostForm(`url`)
+	if url == "" {
+		url = "defaul name for article from article-to-epub software"
+	}
+
+	var f multipart.File
+	f, err = fh.Open()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer f.Close()
+
+	logging.Global.Infof(`File size: %v`, fh.Size)
+	logging.Global.Infof(`Receiver email: %v`, receiverEmail)
+	logging.Global.Infof(`Article name: %v`, url)
+
+	var html []byte
+	html, err = io.ReadAll(f)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	articleName := misc.AdaptUrlToFileName(url)
+	m.convertArticle(c, html, articleName, receiverEmail,
+		&articlesimplifier.ArticleSimplifierFormHTML{}, &htmltoepubconverter.HtmlToEpubConverter{},
+		emailsender.NewEmailSender(""))
 }
 
 func (m *ModuleArticleToEpub) ExposeMethods() {
