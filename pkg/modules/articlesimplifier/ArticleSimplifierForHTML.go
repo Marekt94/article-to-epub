4524980b7bebdea04a6d57f62cc62973d554fc89
Marekt94 <marekt94@gmail.com>
2026-05-15 13:23:42 +0200

    simplifier from html fixed

--- pkg/modules/articlesimplifier/ArticleSimplifierForHTML.go
+++ pkg/modules/articlesimplifier/ArticleSimplifierForHTML.go
@@ -3,6 +3,7 @@ package articlesimplifier
 import (
 	"bytes"
 	"net/url"
+	"os"
 
 	log "github.com/Marekt94/go-kernel-mt/logging"
 	"github.com/go-shiori/go-readability"
@@ -12,7 +13,13 @@ type ArticleSimplifierFormHTML struct {
 }
 
 func (a *ArticleSimplifierFormHTML) SimplifyArticle(article []byte) (simpArticle []byte, title string, author string, err error) {
-	strm := bytes.NewReader(article)
+	path := string(article)
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	strm := bytes.NewReader(b)
 	articleInt, err := readability.FromReader(strm, &url.URL{})
 	if err != nil {
 		return nil, "", "", err
